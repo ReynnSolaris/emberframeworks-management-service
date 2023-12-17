1f979065ae55bc8c9bc7fffa1482955c7e189f4b
ReynnSolaris <>
2023-12-16 19:50:11 -0600

    Initial Push

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmberFrameworksService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : Controller
+{
+    [HttpGet("GetToken_Dev")]
+    public string GetToken_Dev()
+    {
+        return "";
+    }
+}
+
